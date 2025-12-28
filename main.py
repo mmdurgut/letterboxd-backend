4c33fed2e5196c662cf565ead8d03f28ad4614a7
MMD <mmdurgut@hotmail.com>
2025-12-29 00:57:42 +0300

    Add get-user-data endpoint

--- main.py
+++ main.py
@@ -2,29 +2,21 @@ from fastapi import FastAPI, Query
 from rss_parser import get_last_50_movies
 from top_rated_parser import get_top_rated_movies
 
-app = FastAPI(title="Letterboxd Backend")
+app = FastAPI()
 
-@app.get("/get-profile")
-def get_profile(username: str = Query(..., example="mmdurgut")):
-    try:
-        movies = get_last_50_movies(username)
-        return {
-            "username": username,
-            "movie_count": len(movies),
-            "movies": movies
+@app.get("/get-user-data")
+def get_user_data(username: str = Query(...)):
+    recent_movies = get_last_50_movies(username)
+    top_rated_movies = get_top_rated_movies(username)
+
+    return {
+        "username": username,
+        "recent_movies": {
+            "movie_count": len(recent_movies),
+            "movies": recent_movies
+        },
+        "top_rated_movies": {
+            "movie_count": len(top_rated_movies),
+            "movies": top_rated_movies
         }
-    except Exception as e:
-        return {
-            "error": str(e)
-        }
-@app.get("/get-top-rated")
-def get_top_rated(username: str = Query(..., example="mmdurgut")):
-    try:
-        movies = get_top_rated_movies(username)
-        return {
-            "username": username,
-            "movie_count": len(movies),
-            "movies": movies
-        }
-    except Exception as e:
-        return {"error": str(e)}
+    }
